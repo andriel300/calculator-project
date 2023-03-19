080e24af4cdaa0b5485ef035ae02772d608e9ef1
andriel300 <andrielscript@gmail.com>
2023-03-18 23:00:58 -0300

    add calculate compute function and operator chooser

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ const clearBtn = keys.querySelector("[data-action=clear]");
 const deleteButton = keys.querySelector("[data-action=delete]");
 const decimal = keys.querySelector("[data-action=decimal]");
 const equal = keys.querySelector("[data-action=calculate]");
-
-let compute;
+let operation;
 
 const handleClearDisplay = () => {
 	currentDisplay.innerText = "";
@@ -20,6 +19,41 @@ const handleClearDisplay = () => {
 const handleNumberClick = (number) => {
 	if (number === "." && currentDisplay.innerText.includes(".")) return;
 	currentDisplay.innerText += number;
+};
+
+const chooseOperation = (symbol) => {
+	if (currentDisplay.innerText === "") return;
+	calculatorCompute(symbol);
+	operation = symbol;
+	currentDisplay.innerText += symbol;
+	previousDisplay.innerText = currentDisplay.innerText;
+	currentDisplay.innerText = "";
+};
+
+const calculatorCompute = (symbol) => {
+	let result;
+	const secondValue = parseFloat(previousDisplay.innerText);
+	const currentValue = parseFloat(currentDisplay.innerText);
+
+	if (isNaN(secondValue) || isNaN(currentValue)) return;
+
+	switch (operation) {
+		case "+":
+			result = secondValue + currentValue;
+			break;
+		case "-":
+			result = secondValue - currentValue;
+			break;
+		case "×":
+			result = secondValue * currentValue;
+			break;
+		case "÷":
+			result = secondValue / currentValue;
+			break;
+		default:
+			return;
+	}
+	currentDisplay.innerText = result;
 };
 
 // Handle Event Listeners click
@@ -33,4 +67,16 @@ numbers.forEach((number) => {
 deleteButton.addEventListener("click", () => {
 	currentDisplay.innerText = currentDisplay.innerText.slice(0, -1);
 });
+
 clearBtn.addEventListener("click", handleClearDisplay);
+
+operators.forEach((symbol) => {
+	symbol.addEventListener("click", () => {
+		chooseOperation(symbol.innerText);
+	});
+});
+
+equal.addEventListener("click", () => {
+	calculatorCompute();
+	previousDisplay.innerText = "";
+});

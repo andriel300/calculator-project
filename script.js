bc12035a7fc308255ce35a92ed077d5c29b78d60
andriel300 <andrielscript@gmail.com>
2023-03-18 21:55:34 -0300

    add handle number click and clear btn

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 // Select the buttons
 const calculator = document.querySelector(".calculator");
-const display = document.querySelector(".calculator__display");
+const currentDisplay = document.querySelector(".calculator__display");
 const previousDisplay = calculator.querySelector(".calculator__previous");
 const keys = document.querySelector(".calculator__keys");
 const operators = keys.querySelectorAll(".key--operator");
 const numbers = keys.querySelectorAll(".key--number");
-const clear = keys.querySelector("[data-action=clear]");
+const clearBtn = keys.querySelector("[data-action=clear]");
 const deleteButton = keys.querySelector("[data-action=delete]");
 const decimal = keys.querySelector("[data-action=decimal]");
 const equal = keys.querySelector("[data-action=calculate]");
+
+let compute;
+
+const handleClearDisplay = () => {
+	currentDisplay.innerText = "";
+	previousDisplay.innerText = "";
+};
+
+const handleNumberClick = (number) => {
+	if (number === "." && currentDisplay.innerText.includes(".")) return;
+	currentDisplay.innerText += number;
+};
+
+// Handle Event Listeners click
+
+numbers.forEach((number) => {
+	number.addEventListener("click", () => {
+		handleNumberClick(number.innerText);
+	});
+});
+
+clearBtn.addEventListener("click", handleClearDisplay);
